2d75fabbb0a2a4f510a47776cb6a907fd7273974
Jtownokie <christopher.stephens@atlasschool.com>
2024-03-10 21:16:31 -0500

    Completed task 6

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,46 @@
+/* Private Methods and Closures */
+
+class studentHogwarts {
+
+  #privateScore = 0;
+  #name = null;
+
+  #changeScoreBy(points) {
+    this.#privateScore += points;
+  }
+
+  setName(newName) {
+    this.#name = newName;
+  }
+
+  rewardStudent() {
+    this.#changeScoreBy(1);
+  }
+
+  penalizeStudent() {
+    this.#changeScoreBy(-1);
+  }
+
+  getScore() {
+    const objName = this.#name;
+    const objScore = this.#privateScore;
+
+    return {[objName]: objScore};
+  }
+}
+
+const harry = new studentHogwarts()
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = new studentHogwarts()
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
