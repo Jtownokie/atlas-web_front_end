e0ae6d007bdc3b01a125cd6c59cf79bd511db684
Jtownokie <christopher.stephens@atlasschool.com>
2024-03-11 11:48:06 -0500

    Completed task 12

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,11 @@
+/* Practicing Function Binding */
+
+let roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea() {
+    return (this.width * this.length);
+  }
+};
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
